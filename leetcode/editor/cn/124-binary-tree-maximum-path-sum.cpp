82cbb4e6962876d21ea5d4063234e1879c26ac1a
Koschei <nitianzero@gmail.com>
2024-08-28 22:18:31 +0800

    124. 二叉树中的最大路径和

---
+++ leetcode/editor/cn/124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,81 @@
+#include "../../../stdc.h"
+
+using namespace std;
+
+//二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定
+//经过根节点。 
+//
+// 路径和 是路径中各节点值的总和。 
+//
+// 给你一个二叉树的根节点 root ，返回其 最大路径和 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,3]
+//输出：6
+//解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [-10,9,20,null,null,15,7]
+//输出：42
+//解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围是 [1, 3 * 10⁴] 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 2267 👎 0
+
+
+namespace solution124 {
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (root == nullptr) return 0;
+        auto l = max(dfs(root->left), 0);
+        auto r = max(dfs(root->right), 0);
+        max_path_ = max(max_path_, l + r + root->val);
+        return max(l, r) + root->val;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return max_path_;
+    }
+
+private:
+    int max_path_ = INT32_MIN;
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution124;
+
+int main() {
+    Solution solution = Solution();
+    return 0;
+}
