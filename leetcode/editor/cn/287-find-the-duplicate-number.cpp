41a9729d9cf721973c44454fd8cc621596c7e9f1
Koschei <nitianzero@gmail.com>
2024-09-09 23:04:53 +0800

    287. 寻找重复数

---
+++ leetcode/editor/cn/287-find-the-duplicate-number.cpp
@@ -0,0 +1,86 @@
+#include "../../../stdc.h"
+
+using namespace std;
+
+//给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。 
+//
+// 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。 
+//
+// 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,3,4,2,2]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,1,3,4,2]
+//输出：3
+// 
+//
+// 示例 3 : 
+//
+// 
+//输入：nums = [3,3,3,3,3]
+//输出：3
+// 
+//
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 10⁵ 
+// nums.length == n + 1 
+// 1 <= nums[i] <= n 
+// nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 如何证明 nums 中至少存在一个重复的数字? 
+// 你可以设计一个线性级时间复杂度 O(n) 的解决方案吗？ 
+// 
+//
+// Related Topics 位运算 数组 双指针 二分查找 👍 2462 👎 0
+
+
+namespace solution287 {
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution287;
+
+int main() {
+    Solution solution = Solution();
+    return 0;
+}
