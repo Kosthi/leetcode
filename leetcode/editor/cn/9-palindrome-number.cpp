71e64cd2e719c8bd9f868b06c8945db4ba513996
Koschei <nitianzero@gmail.com>
2025-02-07 01:01:12 +0800

    feat: 9. 回文数

---
+++ leetcode/editor/cn/9-palindrome-number.cpp
@@ -0,0 +1,90 @@
+// import universal *.h
+#include "../../../stdc.h"
+
+using namespace std;
+
+
+// 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+//
+//  回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+//
+//
+//  例如，121 是回文，而 123 不是。
+//
+//
+//
+//
+//  示例 1：
+//
+//
+// 输入：x = 121
+// 输出：true
+//
+//
+//  示例 2：
+//
+//
+// 输入：x = -121
+// 输出：false
+// 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+//
+//
+//  示例 3：
+//
+//
+// 输入：x = 10
+// 输出：false
+// 解释：从右向左读, 为 01 。因此它不是一个回文数。
+//
+//
+//
+//
+//  提示：
+//
+//
+//  -2³¹ <= x <= 2³¹ - 1
+//
+//
+//
+//
+//  进阶：你能不将整数转为字符串来解决这个问题吗？
+//
+//  Related Topics 数学 👍 2954 👎 0
+
+
+namespace solution9 {
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+    public:
+        bool isPalindrome(int x) {
+            // std::ostringstream os;
+            // os << x;
+            // std::string s = os.str();
+            // int len = s.size();
+            // for (int i = 0; i < len >> 1; ++i) {
+            //     if (s[i] != s[len - i - 1]) {
+            //         return false;
+            //     }
+            // }
+            // return true;
+            if (x < 0 || x % 10 == 0 && x != 0) {
+                return false;
+            }
+            int reverse = 0;
+            while (x > reverse) {
+                reverse = reverse * 10 + x % 10;
+                x /= 10;
+            }
+            return x == reverse || x == reverse / 10;
+        }
+    };
+    //leetcode submit region end(Prohibit modification and deletion)
+}
+
+using namespace solution9;
+
+int main() {
+    Solution solution = Solution();
+    std::cout << solution.isPalindrome(-121);
+    return 0;
+}
